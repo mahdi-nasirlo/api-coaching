c79b0c9f121804ff6e201a505a05f5cb52a64bb7
morteza-vahdati <132139689+morteza-vahdati@users.noreply.github.com>
2023-07-17 15:37:10 +0330

    blog post resource

--- Modules/Blog/Entities/Post.php
+++ Modules/Blog/Entities/Post.php
@@ -5,10 +5,11 @@ namespace Modules\Blog\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Tags\HasTags;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTags;
 
     protected $guarded = ['created_at', 'updated_at'];
 
