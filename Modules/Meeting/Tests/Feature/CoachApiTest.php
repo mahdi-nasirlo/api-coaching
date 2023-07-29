2fb9a5496d883b17dfa8eb9736d919e8d71e2feb
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-07-29 17:49:55 +0330

    clear test and complete coach api test

--- Modules/Meeting/Tests/Feature/CoachApiTest.php
+++ Modules/Meeting/Tests/Feature/CoachApiTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
 use Modules\Meeting\Entities\Coach;
 use Modules\Meeting\Enums\CoachStatusEnum;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
 
 uses(
@@ -10,9 +12,8 @@ uses(
 );
 
 
-it('return a list of all coach (v1)', function ()
-{
-    $coachPrePage = config('app.pagination_per_page.coach');
+it('return a list of all coach (v1)', function () {
+    $coachPrePage = config('meeting.pagination_per_page.coach');
     Coach::factory()
         ->count(30)
         ->accepted()
@@ -23,4 +24,72 @@ it('return a list of all coach (v1)', function ()
     $res->assertStatus(200);
     $res->assertJsonCount($coachPrePage, 'data');
     $res->assertJsonPath('meta.last_page', Coach::all()->count() / $coachPrePage);
+});
+
+it('user can fill coach data and hold it to accept request', closure: function () {
+
+    $user = App\Models\User::factory()->create();
+    actingAs($user);
+
+    $coachData = [
+        'name' => fake()->name,
+        'hourly_price' => fake()->numberBetween(50000, 1000000),
+        'about_me' => fake()->paragraph,
+        'resume' => fake()->paragraph,
+        'avatar' => UploadedFile::fake()->image('avatar.jpg'),
+    ];
+
+    $res = $this->postJson(route('coach.store'), $coachData);
+    $res->assertStatus(201);
+
+    $user->refresh();
+
+    expect($user->coach->name)->toBe($coachData['name'])
+        ->and($user->coach->coachInfo->about_me)->toBe($coachData['about_me'])
+        ->and($user->coach->coachInfo->resume)->toBe($coachData['resume'])
+        ->and($user->coach->status)->toBe(CoachStatusEnum::PENDING);
+});
+
+it('user cant\'t create coach if last time is make request ', function () {
+    $coach = Coach::factory()->create();
+    actingAs($coach->user);
+
+    $coachData = [
+        'name' => fake()->name,
+        'hourly_price' => fake()->numberBetween(50000, 1000000),
+        'about_me' => fake()->paragraph,
+        'resume' => fake()->paragraph,
+        'avatar' => UploadedFile::fake()->image('avatar.jpg'),
+    ];
+
+    $this->postJson(route('coach.store'), $coachData)
+        ->assertJson(['status' => false])
+        ->assertStatus(403);
+});
+
+it('can update coach info', function () {
+
+    $user = \App\Models\User::factory()->create();
+    actingAs($user);
+
+    $coachData = [
+        'name' => fake()->name,
+        'user_name' => fake()->slug,
+        'hourly_price' => fake()->numberBetween(50000, 1000000),
+        'about_me' => fake()->paragraph,
+        'resume' => fake()->paragraph,
+        'avatar' => UploadedFile::fake()->image('avatar.jpg'),
+    ];
+
+    $res = $this->postJson(route('coach.store'), $coachData)
+        ->assertStatus(201);
+
+    $coachData['name'] = 'test user';
+
+    $update = $this->postJson('api/v1/coach/' . $res->json()['data']['username'], $coachData);
+
+    $coach = $user->refresh()->coach;
+
+    expect($coach->name)->toBe('test user')
+        ->and($coach->status)->toBe(CoachStatusEnum::PENDING);
 });
