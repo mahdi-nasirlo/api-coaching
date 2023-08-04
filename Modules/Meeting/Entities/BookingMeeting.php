c145cddb888c49577115ea33568c9b096cb6bebc
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-04 15:58:44 +0330

    created booking meeting observer

--- Modules/Meeting/Entities/BookingMeeting.php
+++ Modules/Meeting/Entities/BookingMeeting.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Modules\Meeting\Database\factories\BookingMeetingFactory;
+use Modules\Meeting\Observers\BookingMeetingObserver;
 
 class BookingMeeting extends Model
 {
     use HasFactory;
 
     protected $guarded = ['create_at', 'updated_at'];
+
+    protected static function booted(): void
+    {
+        self::observe(BookingMeetingObserver::class);
+    }
 
     protected static function newFactory()
     {
