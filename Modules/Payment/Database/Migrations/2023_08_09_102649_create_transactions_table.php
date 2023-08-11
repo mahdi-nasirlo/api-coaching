7056d472c642eabfae727081e58bed10ffd7d84b
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-11 15:52:13 +0330

    fix transaction entity

--- Modules/Payment/Database/Migrations/2023_08_09_102649_create_transactions_table.php
+++ Modules/Payment/Database/Migrations/2023_08_09_102649_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,11 +14,12 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-
+            $table->unsignedBigInteger('amount');
             $table->boolean('status')->default(false);
-            $table->string('verify_code');
-            $table->string('resnumber');
+            $table->string('verify_code')->nullable();
+            $table->string('resnumber')->nullable();
 
+            $table->morphs('transaction_able');
             $table->timestamps();
         });
     }
--- Modules/Payment/Entities/Transaction.php
+++ Modules/Payment/Entities/Transaction.php
@@ -2,17 +2,36 @@
 
 namespace Modules\Payment\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Modules\Meeting\Entities\Meeting;
+use Modules\Payment\Database\factories\TransactionFactory;
 
+/**
+ * @property string resnumber
+ * @property string verify_code
+ * @property int amount
+ */
 class Transaction extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
-    
+    protected $guarded = ['created_at', 'updated_at'];
+
     protected static function newFactory()
     {
-        return \Modules\Payment\Database\factories\TransactionFactory::new();
+        return TransactionFactory::new();
+    }
+
+    public function transaction_able(): MorphTo
+    {
+        return $this->morphTo('transaction_able', 'transaction_able_type', 'transaction_able_id');
+    }
+
+    public function getMeeting()
+    {
+        return $this->transaction_able_type == Meeting::class ? $this->transaction_able : null;
     }
 }
+
