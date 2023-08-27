d47b2a81671902454b2f12587f4943308b792450
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-27 15:26:30 +0330

    handle cros error

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ResponseAPI;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -15,12 +16,13 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-//        ResponseAPI::class,
+        ResponseAPI::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+//        HandleCors::class,
     ];
 
     /**
--- app/Http/Middleware/ResponseAPI.php
+++ app/Http/Middleware/ResponseAPI.php
@@ -11,9 +11,19 @@ class ResponseAPI
 
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->is('api/*'))
+        if ($request->is('api/*')) {
             $request->headers->set('Accept', 'application/json');
+            $request->headers->set('Access-Control-Allow-Origin', '*');
+            $request->headers->set('Access-Control-Allow-Methods', '*');
+            $request->headers->set('Access-Control-Allow-Credentials', true);
+            $request->headers->set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization');
+        }
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
     }
 }
